9f6e47bf05baf7fd436fed9723884e1c66ef5727
birdilsss-byte <birdilsss@gmail.com>
2026-05-28 21:06:41 +0800

    safety-checkpoint: 20260528-210641

--- stoke/sources/__init__.py
+++ stoke/sources/__init__.py
@@ -12,13 +12,10 @@ from stoke.sources.akshare_source import AKShareSource
 from stoke.sources.tencent_source import TencentSource
 from stoke.sources.baostock_source import BaostockSource
 from stoke.sources.efinance_source import EFinanceSource
-from stoke.sources.zhitu_source import ZhituSource
-
 __all__ = [
     "MootdxSource",
     "AKShareSource",
     "TencentSource",
     "BaostockSource",
     "EFinanceSource",
-    "ZhituSource",
 ]
